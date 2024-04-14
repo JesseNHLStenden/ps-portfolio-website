a51fdd54ff0484b3a4b18ba1f615c68633187ca0
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-04-14 13:09:43 -0700

    Update metadata in layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,12 +7,29 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Bram | Professional Skills Portfolio",
-  description: "Portfolio website voor Professional Skills",
-  icons: {
-    favicon: "/favicon.ico",
+  title: 'Bram | Professional Skills Portfolio',
+  description: 'Welkom bij mijn portfolio website voor Professional Skills. Hier zal ik al mijn documenten op plaatsen die ik in de loop van mijn studie heb gemaakt.',
+  generator: 'Next.js',
+  applicationName: 'Bram | Professional Skills Portfolio',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript', "Tailwind", "Vercel", "Portfolio", "Bram Suurd"],
+  authors: [{ name: 'Bram' }],
+  creator: 'Bram Suurd',
+  publisher: 'Bram Suurd',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
   },
-};
+  metadataBase: new URL('https://ps.bramsuurd.nl'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    title: 'Next.js',
+    description: 'Welkom bij mijn portfolio website voor Professional Skills. Hier zal ik al mijn documenten op plaatsen die ik in de loop van mijn studie heb gemaakt.',
+    url: 'https://bramsuurd.nl/defaultimg.png',
+  },
+}
 
 
 export default function RootLayout({ children }) {
