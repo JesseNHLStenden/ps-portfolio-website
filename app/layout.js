bdfc0bdfdf08a59da06bbbb2a6cda822f14d517b
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-04-14 13:23:24 -0700

    Update metadata in layout.js and add new image URL

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Bram | Professional Skills Portfolio',
-  description: 'Welkom bij mijn portfolio website voor Professional Skills. Hier zal ik al mijn documenten op plaatsen die ik in de loop van mijn studie heb gemaakt.',
   generator: 'Next.js',
   applicationName: 'Bram | Professional Skills Portfolio',
   referrer: 'origin-when-cross-origin',
@@ -22,12 +20,20 @@ export const metadata = {
     telephone: false,
   },
   metadataBase: new URL('https://ps.bramsuurd.nl'),
-  openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    title: 'Next.js',
+    openGraph: {
+  title: 'Bram | Professional Skills Portfolio',
     description: 'Welkom bij mijn portfolio website voor Professional Skills. Hier zal ik al mijn documenten op plaatsen die ik in de loop van mijn studie heb gemaakt.',
     url: '/defaultimg.png',
+    siteName: 'Bram | Portfolio',
+    images: [
+      {
+        url: 'https://bramsuurd.nl/defaultimg.png',
+        width: 1901,
+        height: 916,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
   },
 }
 
