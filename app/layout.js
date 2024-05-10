e6b9915af3d39c429c54e9a5401794bb130f8396
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-10 16:45:56 +0200

    Refactor image loading logic in DocumentShowcase component, remove unused dependencies, and update package.json

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -53,8 +51,6 @@ export default function RootLayout({ children }) {
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <div>
             {children}
-            <Analytics />
-            <SpeedInsights />
           </div>
         </ThemeProvider>
       </body>
