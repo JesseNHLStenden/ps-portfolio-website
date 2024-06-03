e0e180a937a5f22e7483bc5b5194843aa9412306
Bram Suurd <bram.suurd@student.nhlstenden.com>
2024-06-03 11:43:26 +0200

    Added button for student number and added button for website

--- app/layout.js
+++ app/layout.js
@@ -1,41 +1,54 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+import { Toaster } from "@/components/ui/sonner";
+import handleCopy from "@/lib/utils";
+import StudentNumber from "@/components/StudentNumber";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  generator: 'Next.js',
-  applicationName: 'Bram | Professional Skills Portfolio',
-  referrer: 'origin-when-cross-origin',
-  keywords: ['Next.js', 'React', 'JavaScript', "Tailwind", "Vercel", "Portfolio", "Bram Suurd"],
-  authors: [{ name: 'Bram' }],
-  creator: 'Bram Suurd',
-  publisher: 'Bram Suurd',
-  favicon: '/app/favicon.ico',
+  generator: "Next.js",
+  applicationName: "Bram | Professional Skills Portfolio",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Next.js",
+    "React",
+    "JavaScript",
+    "Tailwind",
+    "Vercel",
+    "Portfolio",
+    "Bram Suurd",
+  ],
+  authors: [{ name: "Bram" }],
+  creator: "Bram Suurd",
+  publisher: "Bram Suurd",
+  favicon: "/app/favicon.ico",
   formatDetection: {
     email: false,
     address: false,
     telephone: false,
   },
-  metadataBase: new URL('https://ps.bramsuurd.nl'),
-    openGraph: {
-  title: 'Bram | Professional Skills Portfolio',
-    description: 'Welkom bij mijn portfolio website voor Professional Skills. Hier zal ik al mijn documenten op plaatsen die ik in de loop van mijn studie heb gemaakt.',
-    url: '/defaultimg.png',
-    siteName: 'Bram | Portfolio',
+  metadataBase: new URL("https://ps.bramsuurd.nl"),
+  openGraph: {
+    title: "Bram | Professional Skills Portfolio",
+    description:
+      "Welkom bij mijn portfolio website voor Professional Skills. Hier zal ik al mijn documenten op plaatsen die ik in de loop van mijn studie heb gemaakt.",
+    url: "/defaultimg.png",
+    siteName: "Bram | Portfolio",
     images: [
       {
-        url: 'https://ps.bramsuurd.nl/defaultimg.png',
+        url: "https://ps.bramsuurd.nl/defaultimg.png",
         width: 1901,
         height: 916,
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
-}
-
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -49,7 +62,14 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <div className="select-none">{children}</div>
+          <div className="select-none">
+            {children}
+            <StudentNumber />
+            <Button asChild className="fixed top-0 left-0 m-2" variant="outline">
+              <Link target="_blank" href="https://bramsuurd.nl">Portfolio website</Link>
+            </Button>
+            <Toaster richColors position="bottom-center" />
+          </div>
         </ThemeProvider>
       </body>
     </html>
