1462ef7fea81f37222d9bcf32c08e0394c5243e3
Bram Suurd <bram.suurd@student.nhlstenden.com>
2024-06-06 15:13:14 +0200

    Refactor form layout and error handling in page.js

--- app/page.js
+++ app/page.js
@@ -1,81 +1,9 @@
 import Particles from "@/components/particles";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-function page() {
-=======
-import { useEffect, useState } from "react";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Lock } from "lucide-react";
-import { pb } from "@/lib/pocketbase";
-import { Loader2, BadgeCheck } from "lucide-react";
-
-function Page() {
-  const [token, setToken] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [inputToken, setInputToken] = useState("");
-  const [success, setSuccess] = useState(false);
-
-  useEffect(() => {
-    const { searchParams } = new URL(window.location.href);
-    const tokenitem = searchParams.get("token");
-    if (tokenitem) {
-      setToken(tokenitem);
-    }
-  }, []);
-
-  function clearStates() {
-    setError("");
-    setSuccess(false);
-    token && setToken("");
-  }
-
-  function handleSubmit(event) {
-    console.log("submit");
-    event.preventDefault();
-    verifyToken(inputToken).then((isValid) => {
-      if (isValid) {
-        window.location.href = `/docs?token=${inputToken}`;
-      }
-    });
-  }
-
-  async function verifyToken(token) {
-    setLoading(true);
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const record = await pb
-        .collection("tokens")
-        .getFirstListItem(`token="${token}"`);
-      const isValidToken = record !== undefined && record !== null;
-      setLoading(false);
-      return isValidToken;
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-      return false;
-    }
-  }
-
->>>>>>> parent of 4f55a45 (Refactor form layout and error handling in page.js)
-=======
 import React from "react";
 
 function page() {
->>>>>>> parent of a83590f (added token based authentication to the docs page)
   return (
     <div className="animate-glow flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-tl from-black/20 via-zinc-600/20 to-black/20 antialiased">
       <Particles
@@ -88,80 +16,9 @@ function page() {
         al mijn documenten op plaatsen die ik in de loop van mijn studie heb
         gemaakt.
       </p>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Button size="lg" className="mt-5 animate-fade-in" asChild>
         <Link href="/docs">Bekijk documenten</Link>
       </Button>
-=======
-      <Dialog>
-        <DialogTrigger onClick={clearStates}>
-          {token ? (
-            <Button size="lg" asChild className="mt-5 animate-fade-in">
-              <Link href={`/docs?token=${token}`}>Bekijk documenten</Link>
-            </Button>
-          ) : (
-            <Button size="lg" className="mt-5 animate-fade-in">
-              <Lock className="mr-2 h-4 w-4" />
-              Bekijk documenten
-            </Button>
-          )}
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Authenticatie</DialogTitle>
-            <DialogDescription>
-              Om mijn bestanden te bekijken, moet je een token hebben. heb je
-              deze niet? neem dan contact op met mij.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <form
-              className="flex w-full flex-col gap-2"
-              onSubmit={handleSubmit}
-            >
-              <div className="flex sm:gap-2 gap-4 sm:flex-row flex-col">
-                <Input
-                  type="password"
-                  placeholder="Token"
-                  name="token"
-                  value={inputToken}
-                  onChange={(e) => setInputToken(e.target.value)}
-                
-                  // when the success state is true, the input field will have a green border. when the error state is true, the input field will have a red border.
-                  className={`${
-                    success ? "border-green-500" : ""
-                  } ${error ? "border-red-500" : ""}`}
-                />
-                <Button
-                  className="cursor-pointer"
-                  type="submit"
-                  disabled={loading}
-                >
-                  {loading ? (
-                    <>
-                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />{" "}
-                      <p>Verifieer</p>
-                    </>
-                  ) : (
-                    <>
-                      <BadgeCheck className="mr-2 h-4 w-4" />
-                      <p>Verifieer</p>
-                    </>
-                  )}
-                </Button>
-              </div>
-              {error && <p className="text-xs text-red-500">Verkeerde token, probeer het opnieuw</p>}
-            </form>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
->>>>>>> parent of 4f55a45 (Refactor form layout and error handling in page.js)
-=======
-      <Button size="lg" className="mt-5 animate-fade-in" asChild>
-        <Link href="/docs">Bekijk documenten</Link>
-      </Button>
->>>>>>> parent of a83590f (added token based authentication to the docs page)
     </div>
   );
 }
